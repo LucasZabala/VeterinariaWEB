32085bfd6126fa4615085e5af12c467a649a9a87
Lucas <lucas22nic@gmail.com>
2025-09-20 19:53:03 -0300

    Creacion inicial de Proyectos

---
+++ Veterinaria.Api/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Veterinaria.Api.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
